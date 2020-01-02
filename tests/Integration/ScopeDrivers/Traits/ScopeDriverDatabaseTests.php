a4ad5e33576851102faba70776d6bac68dcb8829
Adam Hutchison <adam.hutchison@netsells.co.uk>
2020-01-02 06:27:06 +0000

    Improves scope driver tests

--- tests/Integration/ScopeDrivers/Traits/ScopeDriverDatabaseTests.php
+++ tests/Integration/ScopeDrivers/Traits/ScopeDriverDatabaseTests.php
@@ -13,13 +13,15 @@ trait ScopeDriverDatabaseTests
      */
     public function within_distance_of_returns_correct_results()
     {
-        $centralPoint = $this->getNearbyLatLongs()->get('central_point');
+        $centralPoint = $this->getLatLongs()->get('central_point');
 
-        $this->createNearbyModels();
+        factory(Test::class, 30)->create();
+        $expected = $this->createNearbyModels(20);
 
-        $actual = Test::withinDistanceOf($centralPoint['latitude'], $centralPoint['longitude'], 20)->get();
+        $actual = Test::withinDistanceOf($centralPoint['latitude'], $centralPoint['longitude'], 10)->get();
 
         $this->assertEquals(20, $actual->count());
+        $this->assertEqualCollections($expected, $actual);
     }
 
     /**
@@ -27,30 +29,21 @@ trait ScopeDriverDatabaseTests
      */
     public function or_within_distance_of_returns_correct_results()
     {
-        $centralPoint = $this->getNearbyLatLongs()->get('central_point');
+        $centralPoint = $this->getLatLongs()->get('central_point');
+        $location2Point = $this->getLatLongs()->get('far_away_points')[0];
 
-        $invalidResult = factory(Test::class)->create([
-            'latitude' => '55.1234',
-            'longitude' => '-67.234'
-        ]);
+        factory(Test::class, 30)->create();
+        $location1Results = $this->createNearbyModels(18);
+        $location2Results = $this->createFarAwayModels(17);
 
-        $this->createNearbyModels();
+        $actual = Test::WithinDistanceOf($centralPoint['latitude'], $centralPoint['longitude'], 1)
+            ->orWithinDistanceOf($location2Point['latitude'], $location2Point['longitude'], 1)
+            ->get();
 
-        $actual = Test::orWithinDistanceOf($centralPoint['latitude'], $centralPoint['longitude'], 20)->get();
+        $expected = $location1Results->merge($location2Results);
 
-        $this->assertEquals(20, $actual->count());
-    }
-
-    protected function createNearbyModels(): void
-    {
-        $latlongs = collect($this->getNearbyLatLongs()->get('nearby_points'));
-
-        factory(Test::class, 20)->create()
-            ->each(function ($testModel) use ($latlongs) {
-                $testModel->latitude = $latlongs->random()['latitude'];
-                $testModel->longitude = $latlongs->random()['longitude'];
-                $testModel->save();
-            });
+        $this->assertEquals(35, $actual->count());
+        $this->assertEqualCollections($expected, $actual);
     }
 
     /**
@@ -122,7 +115,43 @@ trait ScopeDriverDatabaseTests
         $this->assertTrue(in_array(1, $bindings));
     }
 
-    protected function getNearbyLatLongs(): Collection
+    protected function assertEqualCollections(Collection $collection1, Collection $collection2)
+    {
+        $diff1 = $collection1->diff($collection2)->count();
+        $diff2 = $collection2->diff($collection1)->count();
+
+        // Check collection 1 contains no items that aren't in collection 2
+        $this->assertEquals($diff1, 0, "collection 1 contained {$diff1} items that were not in collection 2");
+
+        // Check collection 2 contains no items that are not in collection 1
+        $this->assertEquals($diff2, 0, "collection 2 contained {$diff2} items that were not in collection 2");
+    }
+
+    protected function createNearbyModels(int $numberToCreate = 20): iterable
+    {
+        $latlongs = collect($this->getLatLongs()->get('nearby_points'));
+
+        return factory(Test::class, $numberToCreate)->create()
+            ->each(function ($testModel) use ($latlongs) {
+                $testModel->latitude = $latlongs->random()['latitude'];
+                $testModel->longitude = $latlongs->random()['longitude'];
+                $testModel->save();
+            });
+    }
+
+    protected function createFarAwayModels(int $numberToCreate = 20): iterable
+    {
+        $latlongs = collect($this->getLatLongs()->get('far_away_points'));
+
+        return factory(Test::class, $numberToCreate)->create()
+            ->each(function ($testModel) use ($latlongs) {
+                $testModel->latitude = $latlongs->random()['latitude'];
+                $testModel->longitude = $latlongs->random()['longitude'];
+                $testModel->save();
+            });
+    }
+
+    protected function getLatLongs(): Collection
     {
         return collect([
             'central_point' =>
@@ -130,6 +159,7 @@ trait ScopeDriverDatabaseTests
                     'latitude' => 39.949242,
                     'longitude' => -76.743683
                 ],
+            // All of these points are within 10 miles of the central point
             'nearby_points' =>
                 [
                     [
@@ -162,7 +192,6 @@ trait ScopeDriverDatabaseTests
                     ],
                     [
                         'latitude' => 39.955460,
-                        -76.743899,
                         'longitude' => -76.743899,
                     ],
                     [
@@ -170,6 +199,47 @@ trait ScopeDriverDatabaseTests
                         'longitude' => -76.745734,
                     ],
                 ],
+            // All of these points are around 530 miles away from the central point
+            // And all are less than 540 miles away
+            'far_away_points' =>
+                [
+                    [
+                        'latitude' => 40.950858,
+                        'longitude' => -86.741010,
+                    ],
+                    [
+                        'latitude' => 40.945988,
+                        'longitude' => -86.747788,
+                    ],
+                    [
+                        'latitude' => 40.953489,
+                        'longitude' => -86.736120,
+                    ],
+                    [
+                        'latitude' => 40.953936,
+                        'longitude' => -86.738777,
+                    ],
+                    [
+                        'latitude' => 40.953288,
+                        'longitude' => -86.742254,
+                    ],
+                    [
+                        'latitude' => 40.955550,
+                        'longitude' => -86.753351,
+                    ],
+                    [
+                        'latitude' => 40.954777,
+                        'longitude' => -86.747418,
+                    ],
+                    [
+                        'latitude' => 40.955460,
+                        'longitude' => -86.743899,
+                    ],
+                    [
+                        'latitude' => 40.955460,
+                        'longitude' => -86.745734,
+                    ],
+                ]
         ]);
     }
 }
